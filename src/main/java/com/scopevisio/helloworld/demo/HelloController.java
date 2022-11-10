6e86446ea67a0ac39d4947958ffb823bf3b0a692
Stepan Rutz <stepan.rutz@gmx.de>
2022-11-10 21:04:58 +0000

    more

--- src/main/java/com/scopevisio/helloworld/demo/HelloController.java
+++ src/main/java/com/scopevisio/helloworld/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! testme hi again";
+		return "Greetings from my demo application";
 	}
 
 }
