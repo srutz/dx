3d06d68a1370502475db6fb43ca9fad58b6ab997
Stepan Rutz <stepan.rutz@gmx.de>
2022-11-11 13:54:30 +0000

    mein change

--- src/main/java/com/scopevisio/helloworld/demo/HelloController.java
+++ src/main/java/com/scopevisio/helloworld/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from my demo application";
+		return "Greetings from my demo application ... Test 123";
 	}
 
 }
